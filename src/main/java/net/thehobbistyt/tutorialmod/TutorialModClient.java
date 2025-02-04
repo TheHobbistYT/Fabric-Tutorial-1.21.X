fb6c7ebbbd22e16bde467b1aa32365c3d9675825
TheHobbistYT <mastergamerk1004@gmail.com>
2025-02-04 09:42:22 -0700

    Initial commit

---
+++ src/main/java/net/thehobbistyt/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.thehobbistyt.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
