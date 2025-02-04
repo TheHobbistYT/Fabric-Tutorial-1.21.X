f3153be8e7cdd75ceadb6a121c1622f4c66355cf
TheHobbistYT <mastergamerk1004@gmail.com>
2025-02-04 11:53:53 -0700

    Added pink garnet and raw pink garnet ores, blocks, items, and chisel

--- src/main/java/net/thehobbistyt/tutorialmod/TutorialMod.java
+++ src/main/java/net/thehobbistyt/tutorialmod/TutorialMod.java
@@ -2,17 +2,22 @@ package net.thehobbistyt.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.thehobbistyt.tutorialmod.block.ModBlocks;
+import net.thehobbistyt.tutorialmod.item.ModItemGroups;
+import net.thehobbistyt.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+		ModItemGroups.registerItemGroups();
 
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
